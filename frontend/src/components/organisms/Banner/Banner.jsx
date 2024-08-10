15fd9cb8426071df0a884eafa6616c3e64f85330
odetacocaj <odeta@solution25.com>
2024-08-10 14:02:11 +0200

    .

--- frontend/src/components/organisms/Banner/Banner.jsx
+++ frontend/src/components/organisms/Banner/Banner.jsx
@@ -4,7 +4,7 @@ import "./Banner.scss";
 function Banner() {
   return (
     <div className="banner animated tada">
-      <h1>Adopt, don't shop</h1>
+      <h1 className="big-text">Adopt, don't shop</h1>
       <img src={banner} alt="Banner" className="banner-image" />
     </div>
   );
